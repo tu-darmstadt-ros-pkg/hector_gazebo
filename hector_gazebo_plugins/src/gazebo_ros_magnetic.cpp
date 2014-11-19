63309dbfb5b2894b65866587459faf6398e67171
Johannes Meyer <meyer@fsr.tu-darmstadt.de>
2014-11-19 12:28:23 +0100

    hector_gazebo_plugins: fixed wrong calculation of reference earth magnetic field vector if declination!=0

--- hector_gazebo_plugins/src/gazebo_ros_magnetic.cpp
+++ hector_gazebo_plugins/src/gazebo_ros_magnetic.cpp
@@ -113,8 +113,8 @@ void GazeboRosMagnetic::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
   // Note: Gazebo uses NorthWestUp coordinate system, heading and declination are compass headings
   magnetic_field_.header.frame_id = frame_id_;
   magnetic_field_world_.x = magnitude_ *  cos(inclination_) * cos(reference_heading_ - declination_);
-  magnetic_field_world_.y = magnitude_ *  sin(reference_heading_ - declination_);
-  magnetic_field_world_.z = magnitude_ * -sin(inclination_) * cos(reference_heading_ - declination_);
+  magnetic_field_world_.y = magnitude_ *  cos(inclination_) * sin(reference_heading_ - declination_);
+  magnetic_field_world_.z = magnitude_ * -sin(inclination_);
 
   sensor_model_.Load(_sdf);
 
